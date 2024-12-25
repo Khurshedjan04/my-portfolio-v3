e86defcf5da5b11ff5055c1bca0bb302c5e54de4
Khurshedjan04 <160493817+Khurshedjan04@users.noreply.github.com>
2024-12-26 02:26:23 +0500

    matatags enriched

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,44 @@ import Sidebar from "./components/Sidebar";
 import CursorEffects from "./components/CursorEffects";
 import Bubble from "./components/Bubble";
 export const metadata: Metadata = {
-  title: "Xurshed Alimmamadov",
-  description: "Welcome my next app",
+  title: "Xurshed's portfolio",
+  description:
+    "Welcome to my portflio, Explore a collection of my web development projects, showcasing my skills in front-end technologies.",
   icons: {
     icon: "/logo.svg",
+  },
+  keywords: [
+    "portfolio",
+    "Xurshed",
+    "Alimmamadov",
+    "web development",
+    "front-end",
+    "React",
+    "Next.js,",
+    "Tailwind",
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "Python",
+    "PDP university",
+  ],
+  openGraph: {
+    title: "Xurshed's Portfolio",
+    description:
+      "Explore a collection of web development projects showcasing skills in React, JavaScript, and modern front-end technologies.",
+    url: "https://my-portfolio-v2--seven.vercel.app",
+    siteName: "Xurshed's Portfolio",
+    images: [
+      {
+        url: "/logo.svg",
+        width: 1200,
+        height: 630,
+        alt: "Portfolio Thumbnail",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
   },
 };
 
@@ -20,7 +54,7 @@ export default function RootLayout({
     <html className="scroll-smooth" lang="en">
       <body className="bg-slate-900">
         <CursorEffects />
-        <Bubble/>
+        <Bubble />
         <div className="max-w-screen-xl mx-auto">
           <div
             className="w-full flex flex-col  gap-16 px-6 py-12
